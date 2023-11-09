ef05033f767fd6283ef27740332b12f9205b141b
YongjuLee <ivan.yongju.lee@gmail.com>
2023-11-08 19:23:52 -0800

    add router and bookingpage and homepage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <meta name="description" content="your text goes here"/>
-      <meta name="og:title" content=""/>
-      <meta name="og:description" content=""/>
-      <meta name="og:image" content=""/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/booking" element={<BookingPage />}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
